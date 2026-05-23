7f0d291aeb349ea488ef5390ea70a54480c11af8
Classroomgoogle <tharaniidaranthamizhmani@gmail.com>
2026-05-23 14:33:24 -0400

    Update games.ts

--- src/data/games.ts
+++ src/data/games.ts
@@ -33,7 +33,14 @@ export const games: Game[] = [
     thumbnail: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_yL7AtgrIozF4QESx27DN6CDTvAuxtEfPPw&s',
     category: 'Solo'
   },
-
+  {
+    id: 'game-13',
+    title: 'Proxy',
+    description: 'Allows you to scour the web unblocked',
+    url: 'https://public.rossco.uk/',
+    thumbnail: 'https://cdn.prod.website-files.com/65e50602c9dc544319635b46/65e88c2b238ceb5fe132c177_proxy-logo.png',
+    category: 'Solo'
+  },
   // ── SOLO ──
   {
     id: 'game-3',
@@ -105,14 +112,6 @@ export const games: Game[] = [
     description: 'Jump',
     url: 'https://files.gamezhero.com/game/905/1d1/9051d1173be765fb/data/index.html',
     thumbnail: 'https://upload.wikimedia.org/wikipedia/en/thumb/a/a3/Subway_Surfers_logo.svg/1280px-Subway_Surfers_logo.svg.png',
-    category: 'Solo'
-  },
-  {
-    id: 'game-13',
-    title: 'Proxy',
-    description: 'Allows you to scour the web unblocked',
-    url: 'https://public.rossco.uk/',
-    thumbnail: 'https://cdn.prod.website-files.com/65e50602c9dc544319635b46/65e88c2b238ceb5fe132c177_proxy-logo.png',
     category: 'Solo'
   },
   {
