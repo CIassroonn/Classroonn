a37d3948155773c5878dc6626bedc148991c306b
Classroomgoogle <tharaniidaranthamizhmani@gmail.com>
2026-05-24 20:35:34 -0400

    Update games.ts

--- src/data/games.ts
+++ src/data/games.ts
@@ -355,4 +355,11 @@ export const games: Game[] = [
     thumbnail: 'https://makeitmeme.com/header.webp',
     category: 'Multiplayer'
   },
+  {
+    id: 'game-38',
+    title: 'Gartic Phone Copy',
+    description: '',
+    url: 'https://tharaniidaran-t.github.io/gartic-phone.github/',
+    thumbnail: 'https://static.wikia.nocookie.net/gartic-phone/images/8/87/Garticphone.png/revision/latest/scale-to-width-down/250?cb=20230104102856',
+    category: 'Multiplayer'
 ];
